5d5be53cebb3b8d60b672f49e90b7f8982a488ce
chromecodes <hameedhussain782@gmail.com>
2022-07-12 02:48:55 +0530

    added styles

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
-const modal = document.getElementById("modal")
-const modalWrapper = document.getElementById("modalWrapper")
-const modalOpen = document.getElementById("modalOpen")
-const AddBtn= document.getElementById("add")
+const modal = document.getElementById("modal");
+const modalWrapper = document.getElementById("modalWrapper");
+const modalOpen = document.getElementById("modalOpen");
+const AddBtn= document.getElementById("add");
+
 
 
 let myLibrary = [];
-n = 0 
+n = myLibrary.length 
 
-function Book(title, author, pages) {
+function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.status = status;
 }
 
 function getBook(){
 const title = document.getElementById("title");
 const author = document.getElementById("author");
 const pages = document.getElementById("pages");
+const status = document.getElementById("readSts");
 
-myLibrary[n] = new Book(title.value, author.value, pages.value);
+myLibrary[n] = new Book(title.value, author.value, pages.value, status.checked) ;
 showBook();
 n++;
-title.value = "", author.value="", pages.value="";
+title.value = "", author.value="", pages.value="",status.checked=false;
 modalCtrl();
 }
 
@@ -29,21 +32,13 @@ function showBook(){
   const shelf = document.getElementById("shelf");
   
   const card = document.createElement('div');
-  card.classList.add('card')
+  card.classList.add('card');
   
   const lineOne = document.createElement('div');
   lineOne.classList.add('l-one');
   lineOne.textContent = myLibrary[n].title;
 
   card.appendChild(lineOne);
-
-  const cross = document.createElement('div');
-  cross.classList.add('close');
-  cross.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-  width="12" height="12"
-  viewBox="0 0 24 24"
-  style=" fill:#000000;"><path d="M 4.9902344 3.9902344 A 1.0001 1.0001 0 0 0 4.2929688 5.7070312 L 10.585938 12 L 4.2929688 18.292969 A 1.0001 1.0001 0 1 0 5.7070312 19.707031 L 12 13.414062 L 18.292969 19.707031 A 1.0001 1.0001 0 1 0 19.707031 18.292969 L 13.414062 12 L 19.707031 5.7070312 A 1.0001 1.0001 0 0 0 18.980469 3.9902344 A 1.0001 1.0001 0 0 0 18.292969 4.2929688 L 12 10.585938 L 5.7070312 4.2929688 A 1.0001 1.0001 0 0 0 4.9902344 3.9902344 z"></path></svg>`
-  card.appendChild(cross);
 
   const lineTwo = document.createElement('div');
   lineTwo.classList.add('l-two');
@@ -58,23 +53,51 @@ function showBook(){
   card.appendChild(lineThree);
 
   const delBtn = document.createElement('button');
-  delBtn.classList.add('delete')
+  delBtn.classList.add('delete');
 
   card.appendChild(delBtn);
 
 
   const readed = document.createElement('input');
-  readed.classList.add('readed')
+  readed.classList.add('readed');
 
   card.appendChild(delBtn);
 
+  
+  const stateBg = document.createElement('div');
+  stateBg.classList.add('read-bg');
+
+ 
+  const stateFor = document.createElement('label');
+  stateFor.setAttribute('for', 'read')
+  stateFor.classList.add('read');
+
+  const state = document.createElement('input');
+  state.setAttribute('type', 'checkbox')
+  state.setAttribute('id','read');
+  
+  stateFor.appendChild(state);
+
+  stateBg.appendChild(stateFor);
+
+  card.appendChild(stateBg);
+
   shelf.appendChild(card);
 }
-
+function checkSts(){
+  if(myLibrary[n] == true){
+    t
+  }
+}
 function modalCtrl (){
   modal.classList.toggle('active');             
 }
 
+ const state = document.getElementById('read-bg')
+
+state.addEventListener("click", function(e) {
+  console.log(e)
+})
 
  modalOpen.addEventListener("click", modalCtrl )
  window.addEventListener("click", function(event) {
@@ -84,4 +107,4 @@ function modalCtrl (){
 });
 
 
-AddBtn.addEventListener( "click", getBook ) 
+AddBtn.addEventListener( "click", getBook );
